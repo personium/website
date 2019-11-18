b8c3f4af12e232d7a2bcffabb10f6584b899ffc1
Tochiori, Yasufumi <tochiori.y@jp.fujitsu.com>
2019-11-18 13:43:58 +0900

    remove dosc directory

--- docs/contents/20150206/Japanese/whproj_xml.js
+++
@@ -1,1 +0,0 @@
-gXMLBuffer ="<?xml version='1.0' encoding='utf-8' ?>  <project homepage=\"docs/ja/HomePage.htm\" langid=\"2065\" datapath=\"whxdata\" toc=\"whtoc.xml\" index=\"whidx.xml\" fts=\"whfts.xml\" brs=\"whbrs.xml\" >  </project>";
