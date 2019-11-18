51fc8fdec2b9a544e0038a442cbc6505d0063f9f
Tochiori, Yasufumi <tochiori.y@jp.fujitsu.com>
2019-11-18 17:03:04 +0900

    Modify post_build.sh

--- website/post_build.sh
+++ website/post_build.sh
@@ -3,5 +3,8 @@ BUILD_DIR='./build/personium/'
 
 sed -i \
   -e 's|<title>[^<>]*</title>|<title>News - Personium</title>|g' \
-  -e 's|<meta property="og:title" content=".*" |<meta property="og:title" content="News - Personium" |g' \
   ${BUILD_DIR}blog/{index.html,atom.xml,feed.xml}
+
+sed -i \
+  -e 's|<meta property="og:title" content="[^\"]*"|<meta property="og:title" content="News - Personium" |g' \
+  ${BUILD_DIR}blog/index.html
