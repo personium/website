356f8790ee10fc1a8a860e59548ab17c182902a5
Tochiori, Yasufumi <tochiori.y@jp.fujitsu.com>
2020-02-25 19:56:13 +0900

    Update header links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -43,8 +43,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {href: '/ja/README', label: 'Documentation'},
-    {href: '/ja/apiref/000_Rest_API_Reference', label: 'API Reference'},
+    {href: '/docs/en/README', label: 'Documentation'},
+    {href: '/docs/en/apiref/000_Rest_API_Reference', label: 'API Reference'},
     {blog: true, label: 'News'},
     // {page: 'help', label: 'Help'},
   ],
